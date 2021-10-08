9dd3990b2ffefd002241378a5912daa9d732e742
ServOKio <51263003+ServOKio@users.noreply.github.com>
2021-10-08 03:35:40 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -77,34 +77,7 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'U-18Chan',
-          theme: appTheme.themeData,// ThemeData(
-          //   primaryColor: const Color(0xFF222222), //Header
-          //   fontFamily: 'Arial',
-          //   colorScheme: ColorScheme.fromSwatch(
-          //     cardColor: Colors.orange,
-          //     accentColor: Colors.pink,
-          //     primarySwatch: Colors.green,// idk
-          //     brightness: Brightness.dark //App theme (android)
-          //   ).copyWith(
-          //     secondary: const Color(0xffBBDDEE),
-          //   ),
-          //   scaffoldBackgroundColor: const Color(0xFF1C1C1C),
-          //   textTheme: const TextTheme(
-          //     headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          //     headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          //     bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-          //   ),
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          //   primarySwatch: Colors.red,
-          // ),
+          theme: appTheme.themeData,
           home: Main(),
         )
     );
